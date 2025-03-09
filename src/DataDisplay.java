4e65486683d8705057c9b1796e8cc6e2faa6e355
AceOfKades <knelson17@cub.uca.edu>
2025-03-08 22:54:08 -0600

    Delete src directory
    I am fighting for my life

--- src/DataDisplay.java
+++
@@ -1,9 +0,0 @@
-//GUI Driver
-
-import javax.swing.*;
-
-public class DataDisplay {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(TablePanel::displayTable);
-    }
-}
